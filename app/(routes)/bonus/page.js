8d9fd88ca2dbe8eaaa4d91a0a1dc256569bb1f84
Arshdeep Singh <arsh99.un@gmail.com>
2023-07-03 11:51:27 +0530

    checking bg

--- app/(routes)/bonus/page.js
+++ app/(routes)/bonus/page.js
@@ -40,7 +40,7 @@ export default async function Bonus() {
     let dataArr = Object.values(bonus.data)
 
     return (
-        <div className="linear">
+        <div className="bg-black">
             <Title title='100x Projects' />
             <DataTable data={dataArr} />
         </div>
