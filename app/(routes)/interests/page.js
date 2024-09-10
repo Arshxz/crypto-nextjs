052682759556ddbd1b003bd212b6e9f1e2fae2c2
Arsh <arsh@Arshs-MacBook-Air.local>
2024-09-10 08:42:17 -0400

    Fixed 24% & Updated Interests

--- app/(routes)/interests/page.js
+++ app/(routes)/interests/page.js
@@ -14,7 +14,35 @@ const options = {
 }
 
 async function getInterests() {
-    const res = await fetch(`${HOSTNAME}/v1/cryptocurrency/listings/latest?limit=10`, options)
+    const INTERESTS = [{
+        'WINR': 23681,
+        'COW': 19269,
+        'DINERO': 32492,
+        'PENDLE': 9481,
+        'PERC': 17837,
+        'OP': 11840,
+        'MKR': 1518,
+        'DEGEN': 30096,
+        'PICA': 13678,
+        'FRAX': 6952,
+        'RLB': 15271,
+        'RBN': 12387,
+        'RUNE': 4157,
+        'LPT': 3640,
+        'LAI': 23846,
+        'CRYO': 30965,
+        'GEOD': 20969,
+        'SHRAP': 28363,
+        'PREMIA': 8476,
+        'PRISMA': 28335,
+        'WIFI': 24133
+    }]
+    let list = []
+    INTERESTS.map((item) => (
+        list.push(Object.values(item))
+    ))
+    const string = list.toString()
+    const res = await fetch(`${HOSTNAME}/v2/cryptocurrency/quotes/latest?id=${string}`, options)
     if (!res.ok) {
         throw new Error('Failed to fetch data')
     }
@@ -23,11 +51,12 @@ async function getInterests() {
 
 export default async function Interests() {
     const interests = await getInterests()
+    let dataArr = Object.values(interests.data)
 
     return (
         <div>
             <Title title='Projects I&apos;m looking into' />
-            <DataTable data={interests.data} />
+            <DataTable data={dataArr} />
         </div>
     )
 }
