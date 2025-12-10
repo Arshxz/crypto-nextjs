6cec34306de0b05904371cab96b8c9f103260798
Arsh <Arsh99.in@gmail.com>
2025-12-09 23:33:54 -0500

    downgrade next.js to 15.0.0 for cloudflare compatibility

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ["async_hooks"],
+    // serverComponentsExternalPackages: ["async_hooks"],
   },
 };
 
