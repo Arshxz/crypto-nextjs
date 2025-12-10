3bfc3bc2a2e44192724593636b532abdae816678
Arsh <Arsh99.in@gmail.com>
2025-12-10 00:04:20 -0500

    fix turbopack config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverMinification: false,
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals = config.externals || [];
-      config.externals.push("async_hooks");
-    }
-    return config;
-  },
+  turbopack: {},
 };
 
 export default nextConfig;
